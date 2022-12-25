96e1d8e0118c9d382baa585f69ee2093bce5d9be
KevinLam0 <kevinclam02@gmail.com>
2022-12-25 16:39:04 -0500

    complete js project

--- javascript_portion/lecture.js
+++ javascript_portion/lecture.js
@@ -3,3 +3,15 @@
 // var inKilograms = weight* 0.454
 // alert("That is: " + inKilograms+ " kilograms")
 // console.log("Conversion Completed")
+
+// Part 9 Project 
+var first = prompt("What is your first name?: ")
+var last = prompt("What is your last name?: ")
+var age = prompt("What is your age?: ")
+var height = prompt("How tall are you in cm?: ")
+var pet = prompt("What is your pet's name?: ")
+alert("Thank you for your information.")
+
+if (first[0] == last[0] && 20<age<30 && height>=170 && pet[pet.length-1] == "y") {
+    console.log("You are the spy!");
+}else{console.log("There is nothing to see here...");}
