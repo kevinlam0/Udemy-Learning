cfbc3c0271ec13516544c4d6362840cdef623795
KevinLam0 <kevinclam02@gmail.com>
2022-12-25 16:14:54 -0500

    complete loop exercise

--- javascript_portion/lecture.js
+++ javascript_portion/lecture.js
@@ -1,5 +1,5 @@
 // Part 3 Exercise 
-var weight = prompt("What is the weight in pounds (lbs)? " )
-var inKilograms = weight* 0.454
-alert("That is: " + inKilograms+ " kilograms")
-console.log("Conversion Completed")
+// var weight = prompt("What is the weight in pounds (lbs)? " )
+// var inKilograms = weight* 0.454
+// alert("That is: " + inKilograms+ " kilograms")
+// console.log("Conversion Completed")
