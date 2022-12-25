35101cf380fca695f337072026b2472f0f31c0c3
KevinLam0 <kevinclam02@gmail.com>
2022-12-25 15:18:47 -0500

    javascript exercise

---
+++ javascript_portion/lecture.js
@@ -0,0 +1,5 @@
+// Part 3 Exercise 
+var weight = prompt("What is the weight in pounds (lbs)? " )
+var inKilograms = weight* 0.454
+alert("That is: " + inKilograms+ " kilograms")
+console.log("Conversion Completed")
